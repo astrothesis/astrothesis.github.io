529301c0c2e3b94eb83fcbc0e5483664c60522b1
taseefah <taseefahk@outlook.com>
2025-08-22 16:41:40 +0100

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+// firebaseConfig.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyCFQaMEMZN_KhPyjnn-mTuX5x0clfO13_s",
+  authDomain: "astrothesis.firebaseapp.com",
+  projectId: "astrothesis",
+  storageBucket: "astrothesis.firebasestorage.app",
+  messagingSenderId: "934925526328",
+  appId: "1:934925526328:web:c4ec11838af184c9b28dcd",
+  measurementId: "G-S7R91QYZGR"
+};
