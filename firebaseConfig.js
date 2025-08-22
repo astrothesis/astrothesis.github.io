1f7ff035d1748aa1075a77db1ef23ee28af9b71d
taseefah <taseefahk@outlook.com>
2025-08-22 18:16:33 +0100

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -8,3 +8,14 @@ export const firebaseConfig = {
   appId: "1:934925526328:web:c4ec11838af184c9b28dcd",
   measurementId: "G-S7R91QYZGR"
 };
+
+// firebaseConfig.js
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "astrothesis.firebaseapp.com",
+  projectId: "astrothesis",
+  storageBucket: "astrothesis.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID"
+};
